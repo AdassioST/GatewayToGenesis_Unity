6e447e02939375cfc1ec8d50451809c0ce266dea
Adassio ST <42128563+AdassioST@users.noreply.github.com>
2024-07-03 06:02:31 -0600

    Adding Ages Scriptable Objects
    Added ages as scriptable objects and created the first 5 ages considering name, quote, description, requirements, and crises.

---
+++ Assets/GatewayToGenesis/ScriptableObjects/Ages/AgeSO.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Compilation;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Age", menuName = "Age", order = 2)]
+public class AgeSO : ScriptableObject
+{
+    public int id = 1, age = 1, goldenEraScore, darkEraScore;
+
+    public string ageName, ageQuote = "", ageType = "", eraDescription = "", crisisName = "", crisisDescription = "";
+
+    public bool hasOutcomes;
+        
+    public bool[] crisisTriggers, crisisRequirements;
+}
